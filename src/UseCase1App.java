9c1bf72b76166b1e5b3daad3147f3994c2c4bc28
cc1264 <cc1264@srmist.edu.com>
2026-04-13 09:58:13 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/UseCase1App.java
@@ -0,0 +1,12 @@
+public class UseCase1App {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("WELCOME TO BOOK MY STAY APP");
+        System.out.println("Version : 1.0");
+        System.out.println("=================================");
+
+        System.out.println("Your comfort, our priority!");
+    }
+}
